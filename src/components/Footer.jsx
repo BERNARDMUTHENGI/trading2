08044f0b8c760831428213d92981866c462e0d77
unknown <bernardmuthengi9@gmail.com>
2026-05-26 18:36:06 +0300

    here again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { 
-  Zap, Twitter, Facebook, Instagram, Linkedin, 
-  Mail, Phone, MapPin, Globe, Shield, Award, 
+  Zap, Mail, Phone, MapPin, Globe, Shield, Award, 
   Clock, TrendingUp, LayoutDashboard, Trophy, BookOpen,
-  Sparkles, CreditCard, DollarSign, Headphones, ChevronRight
+  Sparkles, CreditCard, DollarSign, Headphones, ChevronRight,
+  Users, Twitter, Facebook, Instagram, Linkedin
 } from 'lucide-react';
 
 const Footer = () => {
@@ -229,26 +229,5 @@ const Footer = () => {
     </footer>
   );
 };
-
-// Add missing Users icon import
-const Users = (props) => (
-  <svg
-    {...props}
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
-    <circle cx="9" cy="7" r="4" />
-    <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
-    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-  </svg>
-);
 
 export default Footer;
